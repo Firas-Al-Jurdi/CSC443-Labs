00590cfe6a3599c8612bfaafe95d5d21f61abbaf
Firas Al Jurdi <firas.aljurdi@lau.edu>
2021-11-19 17:44:23 +0200

    lab 9 2nd commit

--- WebLab9/server.js
+++ WebLab9/server.js
@@ -51,22 +51,3 @@ app.post('/processForm', upload.single('user_image'), function (req, res) {
 app.listen(PORT, () =>
     console.log(`Started server at http://localhost:${PORT}!`),
 );
-// const express = require('express');
-// const app = express();
-
-// /** Decode Form URL Encoded data */
-// app.use(express.urlencoded());
-
-// /** Show page with a form */
-// app.get('/', (req, res, next) => {
-//   res.send(`<form method="POST" action="/">
-//   <input type="text" name="username" placeholder="username">
-//   <input type="submit">
-// </form>`);
-// });
-
-// /** Process POST request */
-// app.post('/', function (req, res, next) {
-//   res.send(JSON.stringify(req.body.username));
-// });
-// app.listen(3000);
